3895286a16fff1a7611a4f56b328a007eca28a5f
codeman514 <gmail>
2025-10-19 02:44:53 +0800

    +

--- managementScript.js
+++ managementScript.js
@@ -1,6 +1,6 @@
 //載入並顯示客戶聯絡資料
-let sql = `SELECT customerContact.id, customerContact.name AS customerName, customerContact.phone, property.name AS propertyName
-FROM customerContact JOIN property ON customerContact.propertyId = property.id`;
+let sql = `SELECT "customerContact"."id", "customerContact"."name" AS "customerName", "customerContact"."phone", "property"."name" AS "propertyName"
+FROM "customerContact" JOIN property ON "customerContact"."propertyId" = property.id`;
 fetch("/api/getData", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ query: sql }) })
     .then(response => response.json()).then(data => {
         document.getElementById("customerContactData").innerHTML = "";//重置客戶聯絡資料表文字
